a328caaa6f6ed423f4b3e437f2e9b8388203d1f1
e165704 <e165704@ie.u-ryukyu.ac.jp>
2016-12-09 13:21:27 +0900

    step2終わり

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,15 @@
  * Created by e165704 on 2016/12/09.
  */
 public class Main {
-    public static void main(String[] args){
-        String str;
-        str = null;
-        System.out.printf(str);
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }
+        catch (NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しました。");
+            e.printStackTrace();
+            return;
+        }
     }
 }
