8b512ee36a7409df8e6a2de28ec7ec83dfe19410
e165704 <e165704@ie.u-ryukyu.ac.jp>
2016-12-09 12:41:48 +0900

    step1終わり

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by e165704 on 2016/12/09.
+ */
+public class Main {
+    public static void main(String[] args){
+        String str;
+        str = null;
+        System.out.printf(str);
+    }
+}
