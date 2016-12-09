77cc466a43e20be5d416e83e00bae9e903c0817b
e165704 <e165704@ie.u-ryukyu.ac.jp>
2016-12-09 13:39:29 +0900

    step2終わり

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            String str = null;
+            String str = "3.　14";
+            double value = Double.parseDouble(str);
             System.out.println(str.length());
         }
         catch (NullPointerException e){
